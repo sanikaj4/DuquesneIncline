0fa8679f54941a63dc2fa80c34b9f41f763fe211
sanikaj4 <sanikaj@andrew.cmu.edu>
2025-03-17 15:40:04 -0400

    basic start

---
+++ static/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('header nav ul li a');
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
