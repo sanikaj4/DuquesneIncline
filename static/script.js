a93c00636a6a661eb0b21c2336cccb1b0c3361fc
sanikaj4 <sanikaj@andrew.cmu.edu>
2025-03-18 01:53:26 -0400

    s

--- static/script.js
+++ static/script.js
@@ -1,29 +1,4 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
-    
-    dropdownToggles.forEach(toggle => {
-        toggle.addEventListener('click', function(e) {
-            // Prevent the click from propagating to parent elements
-            e.stopPropagation();
-            
-            // Find the parent li element
-            const parentLi = this.closest('li');
-            
-            // Find the dropdown within the parent li
-            const dropdown = parentLi.querySelector('.dropdown');
-            
-            // Toggle the display of the dropdown
-            if (dropdown.style.display === 'block') {
-                dropdown.style.display = 'none';
-            } else {
-                dropdown.style.display = 'block';
-            }
-        });
-    });
-});
-    
-// Close all dropdowns when clicking outside
 document.addEventListener('click', function() {
     const dropdowns = document.querySelectorAll('.dropdown');
     dropdowns.forEach(dropdown => {
