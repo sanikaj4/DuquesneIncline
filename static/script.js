03d37e0ba72e0a685df13e3fc13da82d6d74e16e
sanikaj4 <sanikaj@andrew.cmu.edu>
2025-03-18 01:54:13 -0400

    s

--- static/script.js
+++ static/script.js
@@ -1,12 +1,6 @@
 
-document.addEventListener('click', function() {
-    const dropdowns = document.querySelectorAll('.dropdown');
-    dropdowns.forEach(dropdown => {
-        dropdown.style.display = 'none';
-    });
-});
 function initMap() {
-    var duquesneIncline = { lat: 40.4397, lng: -80.0175 }; // Duquesne Incline coordinates
+    var duquesneIncline = { lat: 40.4397, lng: -80.0175 };
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 15,
         center: duquesneIncline,
