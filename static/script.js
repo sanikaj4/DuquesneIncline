825792842c693a8139f78708ab0646e988e1b1ee
sanikaj4 <sanikaj@andrew.cmu.edu>
2025-03-20 16:45:05 -0400

    change the file a bit

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 
 function initMap() {
-    var duquesneIncline = { lat: 40.4397, lng: -80.0175 };
+    var duquesneIncline = {lat: 40.4397, lng: -80.0175 };
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 15,
         center: duquesneIncline,
