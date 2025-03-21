5ee99695d5c8ae569764b3fc368872bcfe618440
sanikaj4 <sanikaj@andrew.cmu.edu>
2025-03-20 21:12:04 -0400

    history

--- static/script.js
+++ static/script.js
@@ -12,3 +12,13 @@ function initMap() {
         title: "The Duquesne Incline"
     });
 }
+
+document.querySelector(".collapsible").addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+});
