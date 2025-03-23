f902fc9066396888ca4b5c6cc1e738f151dfedf3
sanikaj4 <sanikaj@andrew.cmu.edu>
2025-03-23 13:54:37 -0400

    attractions

--- static/script.js
+++ static/script.js
@@ -24,3 +24,17 @@ $(document).ready(function() {
         $(this).next(".faq-content").toggleClass("active");
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll(".collapsible").forEach(function(button) {
+        button.addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    });
+});
