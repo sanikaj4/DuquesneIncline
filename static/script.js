71472431e93ee7e5b6029323ddeb3603f7e71c7d
sanikaj4 <sanikaj@andrew.cmu.edu>
2025-03-23 01:33:06 -0400

    change

--- static/script.js
+++ static/script.js
@@ -1,11 +1,10 @@
-
 function initMap() {
-    var duquesneIncline = {lat: 40.4397, lng: -80.0175 };
+    var duquesneIncline = {lat: 40.4397, lng: -80.0175};
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 15,
         center: duquesneIncline,
     });
-
+    
     var marker = new google.maps.Marker({
         position: duquesneIncline,
         map: map,
@@ -13,18 +12,15 @@ function initMap() {
     });
 }
 
-document.querySelector(".collapsible").addEventListener("click", function() {
-    this.classList.toggle("active");
-    let content = this.nextElementSibling;
-    if (content.style.display === "flex") {
-        content.style.display = "none";
-    } else {
-        content.style.display = "flex";
-    }
+document.querySelectorAll(".collapsible").forEach(function(button) {
+    button.addEventListener("click", function() {
+        this.classList.toggle("active");
+    });
 });
 
 $(document).ready(function() {
     $(".category-title").click(function() {
         $(this).toggleClass("active");
+        $(this).next(".faq-content").toggleClass("active");
     });
 });
