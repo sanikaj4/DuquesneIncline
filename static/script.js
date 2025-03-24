74dfc8d8017970191ce4c612d1a35a9b0fc77f9f
sanikaj4 <sanikaj@andrew.cmu.edu>
2025-03-24 13:11:03 -0400

    script

--- static/script.js
+++ static/script.js
@@ -1,5 +1,3 @@
-src="https://code.jquery.com/jquery-3.6.0.min.js"
-
 function initMap() {
     var duquesneIncline = {lat: 40.4397, lng: -80.0175};
     var map = new google.maps.Map(document.getElementById("map"), {
